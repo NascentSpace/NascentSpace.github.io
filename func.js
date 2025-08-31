807318266ae728974b411ed577267a0771ad9d26
NascentSpace <60989143+NascentSpace@users.noreply.github.com>
2025-08-31 12:01:28 +1000

    Update

--- func.js
+++ func.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+  $('.page').css('clip-path', 'circle(0% at 50% 50%)');
+  $('.page').first().css('clip-path', 'circle(100% at 50% 50%)');
+  $('.page').first().css('z-index', 2);
+});
+
 function TestsFunction() {
     var T = document.getElementById("TestsDiv");
     if (T.style.display==="none"){
@@ -32,3 +38,27 @@ function ContactFunction() {
 function Fade() {
     ("p").fadeOut()
 }
+
+//Testing zone
+
+$('.page').each(function(i,e){
+  $(this).click(function(event){
+    var x = event.pageX;
+    var y = event.pageY;
+    
+    var nextItem = i + 1;
+    if (nextItem >= $('.page').length){
+      nextItem = 0;
+    }
+    
+    $('.page:eq('+ nextItem +')').css('z-index', parseInt($(this).css('z-index')) + 1);
+    $('.page:eq('+ nextItem +')').css('clip-path', 'circle(0% at '+ x +'px '+ y +'px)');
+    
+    anime({
+      targets: $('.page')[nextItem],
+      update: function(anim) {
+        $('.page:eq('+ nextItem +')').css('clip-path', 'circle('+ (anim.progress*2) +'% at '+ x +'px '+ y +'px)');
+      }
+    });
+  });
+});
